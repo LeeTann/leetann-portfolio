6bf6baebf5157afb2d3d343fa738f6afe50f3eb8
ltann <leeroyleeroy18@gmail.com>
2020-03-21 20:16:12 -0700

    Added start button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaArrowDown, FaArrowUp, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { Link } from 'react-scroll'
 import './hero.css'
 
 export default function Hero() {
@@ -9,8 +11,23 @@ export default function Hero() {
                 <div className="dark-overlay">
                     <div className="landing-inner">
                         <h1 className="hero-heading">Lee Tann</h1>
-                        <h2 className="hero-sub">Full-Stack Developer</h2>      
-                    </div>
+                        <h2 className="hero-sub">Full-Stack Developer</h2>
+                        <h5>
+                          <span><FaArrowUp /></span>
+                          <span><FaArrowUp /></span>
+                          <span><FaArrowDown /></span>
+                          <span><FaArrowDown /></span>
+                          <span><FaArrowLeft /></span>
+                          <span><FaArrowRight /></span>
+                          <span><FaArrowLeft /></span>
+                          <span><FaArrowRight /></span>
+                          <span>B</span>
+                          <span>A</span>
+                          <span>Select</span>
+                          <span>Start</span>
+                        </h5>
+                        <p className="start-btn"><Link to="about" spy={true} smooth={true} offset={-70} duration={700}>START<br/><FaArrowDown /></Link></p>
+                    </div>          
                 </div>
             </section>
         </div>
