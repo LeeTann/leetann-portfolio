886fde1e788efb702f9b737a356d832d0bd4c6ba
ltann <leeroyleeroy18@gmail.com>
2020-08-21 21:03:48 -0700

    Updated about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -17,15 +17,14 @@ export default function About() {
                 <div className="about-section">
                     <Fade left duration="1200">
                         <div className="about-me">
-                            <p>Hi my name is Lee. I'm a full-stack developer based in California. I love building
-                                beautiful and functional apps with cutting edge technologies like
-                                ReactJS, NodeJS, Express, PostgreSQL and MongoDB. My current interest are exploring and working with
-                                cloud services like AWS. I recently received my AWS Cloud Practitioner Certificate and currently
-                                studying for on my AWS Solutions Architect Certificate.
+                            <p>Hi my name is Lee. I'm a full-stack developer based in California. I enjoy building 
+                                functional and aesthetically pleasing apps with cutting edge technologies like
+                                ReactJS, NodeJS, Express, PostgreSQL and MongoDB. I am currently diving into cloud services, building 
+                                serverless web apps and working on obtaining the AWS Solutions Architect Certification. 
                             </p>
                             <p>
-                                Being a software engineer and working with tech in general is amazing as there are endless things to learn
-                                and drive into. I enjoy solving problems by writing code that can make an impact. 
+                                Being a software engineer and working with tech is amazing because it allows me to build software which can have
+                                a huge impact in the world whilst exploring and tinkering with different technologies.
                                 Writing code is a form of expression that allows you to communicate with your machine and with fellow developers.
                                 I believe in the Zen of Python - simple is better than complex. 
                             </p>
