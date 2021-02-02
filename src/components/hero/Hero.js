b64abddabf51d2cab2755e67d69c2a8fedc6bd6d
ltann <leeroyleeroy18@gmail.com>
2021-02-01 17:00:30 -0800

    Updated hero page

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,21 +1,55 @@
 import React from 'react'
 import { FaArrowDown } from 'react-icons/fa'
 import { Link } from 'react-scroll'
+import Typical from 'react-typical'
 import './hero.css'
 
 export default function Hero() {
+  return (
+    <div className='hero'>
+      <section className='landing'>
+        <div className='dark-overlay'>
+          <div className='landing-inner'>
+            <h1 className='hero-heading'>
+              LEE <span>TANN</span>
+            </h1>
+            <p className='hero-sub'>
+              <Typical
+                loop={Infinity}
+                wrapper='b'
+                steps={[
+                  'Software Engineer',
+                  2000,
+                  'Full-Stack Developer',
+                  2000,
+                  'Frontend Developer',
+                  2000,
+                  'Backend Engineer',
+                  2000,
+                  'React Developer',
+                  2000,
+                  'Node.js Developer',
+                  2000,
+                  'Python Developer',
+                  2000,
+                ]}
+              />
+            </p>
 
-    return (
-        <div className="hero">
-            <section className="landing">
-                <div className="dark-overlay">
-                    <div className="landing-inner">
-                        <h1 className="hero-heading">Lee Tann</h1>
-                        <h2 className="hero-sub">Full-Stack Developer</h2>
-                        <p className="start-btn"><Link to="about" spy={true} smooth={true} offset={-70} duration={700}><FaArrowDown /></Link></p>
-                    </div>          
-                </div>
-            </section>
+            <p className='start-btn'>
+              <Link
+                to='about'
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={700}
+              >
+                <FaArrowDown />
+              </Link>
+            </p>
+          </div>
         </div>
-    )
+      </section>
+    </div>
+  )
 }
